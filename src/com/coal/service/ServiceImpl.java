c8f36318ee686e63466e9a4ee267f807a01fa785
ayu <allenyu>
2022-05-18 00:28:09 +0800

    test

--- src/com/coal/service/ServiceImpl.java
+++ src/com/coal/service/ServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import com.coal.service.ServiceImpl.Enitity.Order;
 import com.coal.service.ServiceImpl.Enitity.Order.OrderStatus;
-import com.coal.service.ServiceImpl.Enitity.OrderFlow;
 import com.coal.service.ServiceImpl.Enitity.User;
 import com.coal.service.ServiceImpl.Enitity.User.Role;
 
@@ -27,113 +26,16 @@ public class ServiceImpl {
 		return newOrder;
 	}
 
-	public void assignOrderFlow(Order order, OrderFlow flow) {
-		order.setOrderFlow(flow);
-	}
-
-	public void startOrderFlow(Order order) {
-		order.setStatus(OrderStatus.progressing);
-		order.setCurrentStep(order.getOrderFlow().getRootAction());
-		order.getCurrentStep().getOrderHandler().handleOrder(order);
-	}
-
-	public void moveNextStepOfOrder(Order order) {
-		if (order.getStatus() == OrderStatus.progressing) {
-			order.getCurrentStep().getNextAction().getOrderHandler().handleOrder(order);
-		}
-	}
-
 	public List<Order> queryAllOrdersCreatedByUser(User user, OrderStatus orderStatus) {
 		return Arrays.asList();
 	}
 
 	public static class Enitity {
 
-		public static class OrderFlow {
-			private String id;
-			private String flowName;
-			private OrderStepAction rootAction;
-
-			public OrderFlow(String id, String flowName, OrderStepAction rootAction) {
-				this.id = id;
-				this.flowName = flowName;
-				this.rootAction = rootAction;
-			}
-
-			public String getId() {
-				return id;
-			}
-
-			public void setId(String id) {
-				this.id = id;
-			}
-
-			public String getFlowName() {
-				return flowName;
-			}
-
-			public void setFlowName(String flowName) {
-				this.flowName = flowName;
-			}
-
-			public OrderStepAction getRootAction() {
-				return rootAction;
-			}
-
-			public void setRootAction(OrderStepAction rootAction) {
-				this.rootAction = rootAction;
-			}
-		}
-
 		public static interface OrderHandler {
 			public Order handleOrder(Order order);
 		}
 
-		public static class OrderStepAction {
-			private String id;
-			private String actionName;
-			private OrderHandler orderHandler;
-			private OrderStepAction nextAction;
-			private OrderStepAction previousAction;
-
-			public OrderStepAction(String id, String actionName) {
-				this.setId(id);
-				this.setActionName(actionName);
-			}
-
-			public String getId() {
-				return id;
-			}
-
-			public void setId(String id) {
-				this.id = id;
-			}
-
-			public String getActionName() {
-				return actionName;
-			}
-
-			public void setActionName(String actionName) {
-				this.actionName = actionName;
-			}
-
-			public OrderStepAction getNextAction() {
-				return nextAction;
-			}
-
-			public OrderStepAction getPreviousAction() {
-				return previousAction;
-			}
-
-			public OrderHandler getOrderHandler() {
-				return orderHandler;
-			}
-
-			public void setOrderHandler(OrderHandler orderHandler) {
-				this.orderHandler = orderHandler;
-			}
-
-		}
 
 		public static class Order {
 			public static enum OrderStatus {
@@ -143,8 +45,7 @@ public class ServiceImpl {
 			private String id;
 			private User createdUser;
 			private OrderStatus status;
-			private OrderFlow orderFlow;
-			private OrderStepAction currentStep;
+
 
 			public Order(String id, User createdUser) {
 				super();
@@ -175,22 +76,6 @@ public class ServiceImpl {
 
 			public void setStatus(OrderStatus status) {
 				this.status = status;
-			}
-
-			public OrderFlow getOrderFlow() {
-				return orderFlow;
-			}
-
-			public void setOrderFlow(OrderFlow orderFlow) {
-				this.orderFlow = orderFlow;
-			}
-
-			public OrderStepAction getCurrentStep() {
-				return currentStep;
-			}
-
-			public void setCurrentStep(OrderStepAction currentStep) {
-				this.currentStep = currentStep;
 			}
 
 		}
