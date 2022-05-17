6ce005782691d28175afa24b2b206052d9417110
ayu <allen.yu@aspiraconnect.com>
2022-05-17 13:31:50 +0800

    test

---
+++ src/com/coal/service/ServiceImpl.java
@@ -0,0 +1,164 @@
+package com.coal.service;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.List;
+
+import com.coal.service.ServiceImpl.Enitity.Order;
+import com.coal.service.ServiceImpl.Enitity.Order.OrderStatus;
+import com.coal.service.ServiceImpl.Enitity.User;
+import com.coal.service.ServiceImpl.Enitity.User.Role;
+
+public class ServiceImpl {
+
+	public  User createNewUser(String id, String name, String projectName, Calendar projectDeliverDate) {
+		User newUser = new User(id, name, projectName, projectDeliverDate,Role.User);
+		return newUser;
+	}
+	
+	public  User createNewOperator(String id, String name) {
+		User newOperator = new User(id, name, Role.Operator);
+		return newOperator;
+	}
+
+	public  Order createNewOrder(User user, String id) {
+		Order newOrder = new Order(id, user);
+		return newOrder;
+	}
+	
+	public  void acceptOrder(User user, Order order) {
+		
+	}
+	
+	public  List<Order> queryAllOrdersCreatedByUser(User user, OrderStatus orderStatus){
+		return Arrays.asList();
+	}
+
+	public static class Enitity {
+		public static class Order {
+			public static enum OrderStatus {
+				new_created, rejected, completed 
+			}
+
+			private String id;
+			private User createdUser;
+			private OrderStatus status;
+
+			public Order(String id, User createdUser) {
+				super();
+				this.id = id;
+				this.createdUser = createdUser;
+				this.status = OrderStatus.new_created;
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(String id) {
+				this.id = id;
+			}
+
+			public User getCreatedUser() {
+				return createdUser;
+			}
+
+			public void setCreatedUser(User createdUser) {
+				this.createdUser = createdUser;
+			}
+
+			public OrderStatus getStatus() {
+				return status;
+			}
+
+			public void setStatus(OrderStatus status) {
+				this.status = status;
+			}
+
+		}
+
+		public static class User {
+
+			public static enum UserStatus {
+				new_created, approved
+			}
+			
+			public static enum Role{
+				User, Operator, Admin
+			}
+
+			private String name;
+			private String projectName;
+			private Calendar projectDeliverDate;
+			private String id;
+			private UserStatus status;
+			private Role role;
+
+			public User() {
+				this.status = UserStatus.new_created;
+			}
+
+			public User(String id, String name, String projectName, Calendar projectDeliverDate,Role role) {
+				super();
+				this.id = id;
+				this.name = name;
+				this.projectName = projectName;
+				this.projectDeliverDate = projectDeliverDate;
+				this.role = role;
+			}
+
+			public User(String id2, String name2, Role operator) {
+				// TODO Auto-generated constructor stub
+			}
+
+			public String getId() {
+				return id;
+			}
+
+			public void setId(String id) {
+				this.id = id;
+			}
+
+			public String getName() {
+				return name;
+			}
+
+			public void setName(String name) {
+				this.name = name;
+			}
+
+			public String getProjectName() {
+				return projectName;
+			}
+
+			public void setProjectName(String projectName) {
+				this.projectName = projectName;
+			}
+
+			public Calendar getProjectDeliverDate() {
+				return projectDeliverDate;
+			}
+
+			public void setProjectDeliverDate(Calendar projectDeliverDate) {
+				this.projectDeliverDate = projectDeliverDate;
+			}
+
+			public UserStatus getStatus() {
+				return status;
+			}
+
+			public void setStatus(UserStatus status) {
+				this.status = status;
+			}
+
+			public Role getRole() {
+				return role;
+			}
+
+			public void setRole(Role role) {
+				this.role = role;
+			}
+		}
+	}
+
+}
