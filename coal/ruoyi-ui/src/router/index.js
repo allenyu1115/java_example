824e0de63dd6a23f63d8c23974f42ea99d6cb5bc
ayu <allenyu>
2022-05-22 15:30:29 +0800

    test

--- coal/ruoyi-ui/src/router/index.js
+++ coal/ruoyi-ui/src/router/index.js
@@ -165,7 +165,7 @@ export const dynamicRoutes = [
 ]
 
 // 防止连续点击多次路由报错
-let routerPush = Router.prototype.push;
+const routerPush = Router.prototype.push
 Router.prototype.push = function push(location) {
   return routerPush.call(this, location).catch(err => err)
 }
