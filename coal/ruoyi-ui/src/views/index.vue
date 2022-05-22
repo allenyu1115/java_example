f3157b7dcfaf17c0c891e28c408cda8f845b58da
ayu <allenyu>
2022-05-22 11:07:10 +0800

    test

---
+++ coal/ruoyi-ui/src/views/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-container home">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Index",
+  data() {
+    return {
+      // 版本号
+      version: "3.8.2",
+    };
+  },
+  methods: {
+    goTarget(href) {
+      window.open(href, "_blank");
+    },
+  },
+};
+</script>
+
+
+
---
+++ coal/ruoyi-ui/startUI.bat
@@ -0,0 +1,2 @@
+C:\Windows\System32\cmd.exe /k "C:\Program Files\nodejs\nodevars.bat"
+pause
